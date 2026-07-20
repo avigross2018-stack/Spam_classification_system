322689ab04c25438c25b6f57e7d22fa1077093f1
avi-gross <avigross2018@gmail.com>
2026-07-20 17:24:12 +0300

    base folder struct

---
+++ SpamClassificationSystem/src/interfaces/ITrainer.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpamClassificationSystem.src.interfaces
+{
+    public interface ITrainer
+    {
+        
+    }
+}
