b3a823370291b213e56fe2c2faf678d27551e2ad
Netanel Pardes <sendi8475@gmail.com>
2026-07-21 12:12:00 +0300

    .

--- SpamClassificationSystem/Program.cs
+++ SpamClassificationSystem/Program.cs
@@ -1,19 +1,19 @@
 ﻿using DroneFleetDataProcessing.src;
+using SpamClassificationSystem.src.interfaces;
 using SpamClassificationSystem.src.models;
 using SpamClassificationSystem.src.utils;
 using SpamClassificationSystem.src.workflow;
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace SpamClassificationSystem
 {
     class Program
     {
-        static void Main(string[] args)
+        static void test_func(string path)
         {
             CSVReader reade = new CSVReader();
-            PathManager path = new PathManager();
-            //test 
-            DataSet test = reade.Read(path.getInputPath("weather_play.csv"));
+            DataSet test = reade.Read(path);
             for (int i = 0; i < test.GetLabels().Count; i++)
             {
                 Console.WriteLine(test.GetLabels()[i]);
@@ -32,6 +32,15 @@ namespace SpamClassificationSystem
                 Console.WriteLine();
                 Console.WriteLine(item);
             }
+        }
+        static void Main(string[] args)
+        {
+            CSVReader reade = new CSVReader();
+            PathManager path = new PathManager();
+            //test 
+            test_func(path.getInputPath("weather_play.csv"));
+            
+            
         }
     }
 }
