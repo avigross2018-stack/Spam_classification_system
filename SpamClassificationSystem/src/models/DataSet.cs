e461335063b6c58383f073c4bc716514d630f336
Netanel Pardes <sendi8475@gmail.com>
2026-07-21 09:46:15 +0300

    add a interfase reder and csvreader

--- SpamClassificationSystem/src/models/DataSet.cs
+++ SpamClassificationSystem/src/models/DataSet.cs
@@ -8,10 +8,10 @@ namespace SpamClassificationSystem.src.models
     public class DataSet
     {
         private List<string> _labels { get; set;}
-        private Dictionary<string, string> _rows {get; set;}
+        private List<Dictionary<string, string>> _rows {get; set;}
         private List<string> _target { get; set;}
 
-        DataSet(List<string> labels, Dictionary<string, string> rows, List<string> target)
+        DataSet(List<string> labels, List<Dictionary<string, string>> rows, List<string> target)
         {
             _labels = labels;
             _rows = rows;
